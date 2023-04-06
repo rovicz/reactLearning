1aaecd394ddaa88d7c97c8fb318a2de8fa1f4d7e
rovicz <roviczzf@gmail.com>
2023-04-06 15:30:04 -0300

    mais react - useReducer

--- mais-react/appreact/src/App.js
+++ mais-react/appreact/src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import Button from "./Button";
 import Header from "./Header";
 
+function reducer(state, action) {
+  switch (action) {
+    case "aumentar":
+      return state + 1;
+    case "reduzir":
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
 const App = () => {
   const [contar, setContar] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, 0);
 
   function handleClick() {
     setContar(contar + 1);
@@ -15,6 +27,10 @@ const App = () => {
       <Button onClick={handleClick} margin="20px" width={240}>
         {contar}
       </Button>
+
+      <button onClick={() => dispatch("aumentar")}>+</button>
+      <button onClick={() => dispatch("diminuir")}>-</button>
+      <p>{state}</p>
     </div>
   );
 };
