f7287391dea6b249e7f03eb431a9e3b450846843
Victor Fernandes <roviczzf@gmail.com>
2023-07-19 14:51:48 -0300

    final project - validate token 2/2

--- final-project/src/Components/Header.jsx
+++ final-project/src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -15,7 +15,7 @@ const Header = () => {
         </Link>
         {data ? (
           <Link className={styles.login} to="/conta">
-            Bem vindo, {data.nome}.
+            Bem vindo, {data.nome}.<button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
