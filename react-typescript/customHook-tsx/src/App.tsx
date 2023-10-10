d55823ce2b71a7ee27821d05469b2eef25be99e9
Victor Fernandes <roviczzf@gmail.com>
2023-10-10 19:52:43 -0300

    reactTS - customHook 2/2

--- react-typescript/customHook-tsx/src/App.tsx
+++ react-typescript/customHook-tsx/src/App.tsx
@@ -9,15 +9,18 @@ function App() {
   React.useEffect(() => {
     if (!video.current) return;
     const n = Number(volume);
-    if (n >= 0 && n <= 1) video.current.volume = n;
+    if (n >= 0 && n <= 1) {
+      video.current.volume = n;
+    }
   }, [volume]);
   return (
     <div>
       {volume}
-      <div className="flex">
+      <div className="flex buttons">
         <button onClick={() => setVolume("0")}>0</button>
         <button onClick={() => setVolume("0.5")}>50</button>
         <button onClick={() => setVolume("1")}>100</button>
+        <button onClick={() => setVolume("2")}>200 (Erro)</button>
       </div>
       <video controls ref={video} src={videoSrc}></video>
     </div>
