3f4d40e6287ec871cde2d3acdaf5ad68031b17e7
Victor Fernandes <roviczzf@gmail.com>
2023-10-10 17:28:04 -0300

    reactTS - customHook 1/2

---
+++ react-typescript/customHook-tsx/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import videoSrc from "./video.mp4";
+import useLocalStorage from "./useLocalStorage";
+
+function App() {
+  const [volume, setVolume] = useLocalStorage("volume", "0");
+  const video = React.useRef<HTMLVideoElement>(null);
+
+  React.useEffect(() => {
+    if (!video.current) return;
+    const n = Number(volume);
+    if (n >= 0 && n <= 1) video.current.volume = n;
+  }, [volume]);
+  return (
+    <div>
+      {volume}
+      <div className="flex">
+        <button onClick={() => setVolume("0")}>0</button>
+        <button onClick={() => setVolume("0.5")}>50</button>
+        <button onClick={() => setVolume("1")}>100</button>
+      </div>
+      <video controls ref={video} src={videoSrc}></video>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-typescript/customHook-tsx/src/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const useLocalStorage = (
+  key: string,
+  inicial: string,
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const [state, setState] = React.useState(() => {
+    const local = window.localStorage.getItem(key);
+    return local ? local : inicial;
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [state, key]);
+
+  return [state, setState];
+};
+
+export default useLocalStorage;
