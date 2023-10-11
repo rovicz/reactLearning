66b9d1a2a1ca22e2f50e4a6d334dd6c798412553
Victor Fernandes <roviczzf@gmail.com>
2023-10-11 17:04:14 -0300

    reactTS - useFetch 3/3

--- react-typescript/usefetch-stx/src/App.tsx
+++ react-typescript/usefetch-stx/src/App.tsx
@@ -11,9 +11,36 @@ type Produto = {
 };
 
 function App() {
-  // const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos/");
+  const [id, setId] = React.useState("p001");
+  const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos/");
+  const produto = useFetch<Produto>(`https://data.origamid.dev/produtos/${id}`);
 
-  return <div></div>;
+  return (
+    <section className="flex">
+      <div>
+        {produtos.data &&
+          produtos.data.map((produto) => (
+            <button
+              onClick={() => setId(produto.id)}
+              style={{ fontSize: "1rem" }}
+              key={produto.id}
+            >
+              {produto.id}
+            </button>
+          ))}
+      </div>
+      <div>
+        {produto.data && (
+          <ul>
+            <li>ID: {produto.data.id}</li>
+            <li>Nome: {produto.data.nome}</li>
+            <li>Descrição: {produto.data.descricao}</li>
+            <li>Quantidade: {produto.data.quantidade}</li>
+          </ul>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default App;
